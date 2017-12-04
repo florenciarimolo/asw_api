c476bc9fca12ed0633481c2f86b1a24a4bf5aa3a
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2017-12-04 20:07:16 +0100

    prepare for heroku

--- django_api/prod_settings.py
+++ django_api/prod_settings.py
@@ -1,10 +1,7 @@
-import dj_database_url
-import os
-
-import settings
+from settings import *
 
 
-settings.SECRET_KEY = os.environ.get('SECRET_KEY')
-settings.DEBUG = False
-settings.DATABASES['heroku'] = dj_database_url.config()
-settings.DATABASES['heroku']['CONN_MAX_AGE'] = 500
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = False
+DATABASES['heroku'] = dj_database_url.config()
+DATABASES['heroku']['CONN_MAX_AGE'] = 500
